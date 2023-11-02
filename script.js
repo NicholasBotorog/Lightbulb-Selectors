cad06f5d524e03858a5089989ce3b83bc033eda7
Nicholas Botorog <nicholasbotorog@gmail.com>
2023-11-01 23:37:57 -0400

    finish without the challenge puzzle

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Write your code here
+const bulb1 = document.querySelector('#lightbulb1')
+const bulb2 = document.querySelector('#lightbulb2')
+const bulb3 = document.querySelector('#lightbulb3')
+const lights = document.querySelectorAll('.items')
+const h3 = document.querySelector('.subtitle')
+let count = 0
+
+h3.innerHTML = `You've clicked the lightbulbs ${count} time`
+
+const handleClick = function(){
+    count += 1
+    if(count <= 1){
+         h3.innerHTML = `You've clicked the lightbulbs ${count} time`
+    } else {
+        h3.innerHTML = `You've clicked the lightbulbs ${count} times`
+    }
+    this.classList.toggle('active')
+}
+
+bulb1.addEventListener('mouseenter', handleClick)
+bulb2.addEventListener('mouseenter', handleClick)
+bulb3.addEventListener('mouseenter', handleClick)
